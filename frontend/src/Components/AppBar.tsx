685bab4052282348288611ec2ed838c45439e5fe
Vani <vanichaudhary0150@gmail.com>
2025-09-06 22:39:47 +0530

    Publish button added

--- frontend/src/Components/AppBar.tsx
+++ frontend/src/Components/AppBar.tsx
@@ -10,6 +10,9 @@ export const AppBar = ()=>{
                 <BlabberLogo height={36} colorStart="#374151" colorEnd="#111827"  />
                 </Link>
             </div>
+              <Link to={`/publish`}>
+                <button type="button" className="mr-4 text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 ">New</button>
+            </Link>
             <div>
                 <Avatar size="big" name="vani"/>
             </div>
